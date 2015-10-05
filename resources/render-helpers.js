d8f828cdf238f0273b744c5e71de9f7f24be310a
Ben Barber <barberboy@gmail.com>
2015-10-05 03:19:15 +0000

    Add index.html page and styles

--- resources/render-helpers.js
+++ resources/render-helpers.js
@@ -1,7 +1,5 @@
 function displayAnimalGallery(animals) {
-    console.log(animals);
-
-    var container = document.querySelector('.animal-gallery');
+    var container = document.querySelector('.animal-gallery--animals');
 
     animals.map(animalDetails).forEach(function(element) {
         container.appendChild(element);
@@ -9,67 +7,70 @@ function displayAnimalGallery(animals) {
 }
 
 function displayFeaturedAnimal(animal) {
-    console.log(animal);
-    var container = document.querySelector('.featured-animal');
-    container.innerHTML += animal;
+    var container = document.querySelector('.featured-animal--info');
+
+    var name = container.querySelector('.featured-animal--name');
+    name.appendChild(document.createTextNode(animal.name));
+
+    var location = container.querySelector('.featured-animal--location');
+    location.appendChild(document.createTextNode(animal.location));
+
+    var age = container.querySelector('.featured-animal--age');
+    age.appendChild(document.createTextNode(animal.age));
+
+    var commonName = container.querySelector('.featured-animal--common-name');
+    commonName.appendChild(document.createTextNode(animal.commonName));
+
+    var imageContainer = container.querySelector('.featured-animal--image-container');
+
+    var image = document.createElement('img');
+    image.className = 'featured-animal--image';
+    image.src = 'images/' + animal.image;
+
+    imageContainer.appendChild(image);
 }
 
 function displayAnimalAges(ages) {
-    console.log(ages);
     var container = document.querySelector('.animal-ages');
     var table = document.createElement('table');
     var headingRow = document.createElement('tr');
-    var nameHeading = document.createElement('th');
 
+    var nameHeading = document.createElement('th');
     nameHeading.appendChild(document.createTextNode('Name'));
     headingRow.appendChild(nameHeading);
+    
     var speciesHeading = document.createElement('th');
     speciesHeading.appendChild(document.createTextNode('Species'));
     headingRow.appendChild(speciesHeading);
+    
     var ageHeading = document.createElement('th');
     ageHeading.appendChild(document.createTextNode('Age'));
     headingRow.appendChild(ageHeading);
     table.appendChild(headingRow);
 
+    ages.map(ageDetails).forEach(function(details) {
+        table.appendChild(details);
+    });
     container.appendChild(table);
 
-    ages.map(ageDetails).forEach(function(details) {
-        container.appendChild(details);
-    });
 }
 
 function animalDetails(animal) {
     var element = document.createElement('article');
-
-    var name = document.createElement('h3');
-    name.className = 'name';
-    name.appendChild(document.createTextNode(animal.name));
-    element.appendChild(name);
+    element.className = 'animal';
+    element.appendChild(createElement('h3', 'animal--name', animal.name));
 
     var image = document.createElement('img');
-    image.className = 'image';
+    image.className = 'animal--image';
     image.src = 'images/' + animal.image;
     element.appendChild(image);
 
-    var commonName = document.createElement('div');
-    commonName.className = 'common-name';
-    commonName.appendChild(document.createTextNode(animal.commonName));
-    element.appendChild(commonName);
-
-    var species = document.createElement('div');
-    species.className = 'species';
-    species.appendChild(document.createTextNode(animal.species));
-    element.appendChild(species);
-
-    var location = document.createElement('div');
-    location.className = 'location';
-    location.appendChild(document.createTextNode(animal.location));
-    element.appendChild(location);
-
-    var age = document.createElement('div');
-    age.className = 'age';
-    age.appendChild(document.createTextNode(animal.age));
-    element.appendChild(age);
+    var details = createElement('div', 'animal--details');
+    details.appendChild(createLabelAndValue('animal--common-name', 'Common name', animal.commonName));
+    details.appendChild(createLabelAndValue('animal--species', 'Species', animal.species));
+    details.appendChild(createLabelAndValue('animal--location', 'Location', animal.location));
+    details.appendChild(createLabelAndValue('animal--age', 'Age', animal.age));
+    element.appendChild(details);
 
     return element;
 }
@@ -77,20 +78,29 @@ function animalDetails(animal) {
 function ageDetails(animal) {
     var row = document.createElement('tr');
 
-    var name = document.createElement('td');
-    name.className = 'name';
-    name.appendChild(document.createTextNode(animal.name));
-    row.appendChild(name);
-
-    var commonName = document.createElement('td');
-    commonName.className = 'common-name';
-    commonName.appendChild(document.createTextNode(animal.commonName));
-    row.appendChild(commonName);
-
-    var age = document.createElement('td');
-    age.className = 'age';
-    age.appendChild(document.createTextNode(animal.age));
-    row.appendChild(age);
+    row.appendChild(createElement('td', 'name', animal.name));
+    row.appendChild(createElement('td', 'common-name', animal.commonName));
+    row.appendChild(createElement('td', 'age', animal.age));
 
     return row;
+}
+
+function createLabelAndValue(className, label, text) {
+    var element = createElement('div', className);
+    element.appendChild(createLabel(label));
+    element.appendChild(document.createTextNode(' '));
+    element.appendChild(createElement('span', 'value', text));
+    return element;
+}
+
+function createLabel(text) {
+    return createElement('span', 'label', text);
+}
+
+function createElement(tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.className = className;
+    element.appendChild(document.createTextNode(text || ''));
+
+    return element;
 }
