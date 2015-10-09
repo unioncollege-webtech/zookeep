1decfba5398edff8aa82f2c1fb6ee8cff0214fab
Matilda Kond <tmiacadet@gmail.com>
2015-10-09 18:46:22 +0000

    Problems with sort and Display

---
+++ zoo.js
@@ -0,0 +1,19 @@
+
+var animals =
+{
+    name: ['Mooney','Sami', 'Chester', 'Scarborough', 'Maverick','Puck', 'Tweed', 'Taylor', 'Trip', 'Helios', 'Asteria', 'Perses', 'Pip', 'Skrytnaya','Matadi', 'Kimani', 'Antipode'],
+    commonName: ['Blue monkey', 'Common squirrel monkey', 'Black howler monkey', 'Scarlet macaw', 'Tufted puin', 'Little penguin', 'Leafy sea dragon','Swift Fox','Cape thick-kneee', 'Mantied guereza','Western lowland gorilla','Wolfs mona monkey','Red river hog', 'Amur leapard',' African lion', 'Siberian tiger', 'Polar bear'],
+    species: ['Cercopithecus','Saimiri sciureus', 'Alouatta caraya','Ara macao','Fratercula cirrhata','Eudyptula minor', 'Phycodurus eques','Vulpes velox','Burhinus capendsus','Colobus guerza', 'Gorilla gorilla gorilla', 'Cercopitcus wolfi','Potamochoerus porcus','Panthera pardus orientails', 'Panthero leo','Panthera tigris altaica', 'Ursus maritimus'],
+    location: ['Smith Jungle','Birai Aquarium',' Rodeheaver Desert', 'Fagan Valley', 'Kond Cat Complex', 'Moser Canyon'],
+    age: [2, 2, 5, 3, 2, 4, 1, 9, 5, 11, 10, 4, 8, 6, 8, 11],
+    image: ['Blue_monkey.jpg','Common_squirrel_monkey.jpg', 'Black_howler_monkey.jpg','Scarlet_macaw.jpg','Tufted_puffin.jpg','Little_penguin.jpg','Leafy_seadragon.jpg','Swift_fox.jpg','Cape_thick-knee.jpg','Mantled_guereza.jpg','Western_lowland_gorilla.jpg','Wolfs_mona_monkey.jpg','Red_river_hog.jpg','Amur_leopard.jpg','African_lion.jpg','Siberian_tiger.jpg','Polar_bear.jpg']
+};
+
+
+
+animals.name.sort(function displayAnimalGallery(animals)
+{
+    return (animals);
+});
+
+
