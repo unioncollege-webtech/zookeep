2bfdc02ac4a6fa3954591290e4228a42df5a4ca0
moser2010 <cmoser351@gmail.co>
2015-10-09 20:19:02 +0000

    objects made

---
+++ zookee.js
@@ -0,0 +1,140 @@
+var animals = {
+    
+};
+
+var mooney = {
+    name: "Mooney",
+    commonName: "Blue monkey",
+    species: "Cercopithecus mitis",
+    location: "Smith Jungle",
+    age: 2,
+    image: Blue_monkey.jpg
+}
+var Sami = {
+    name: "Sami",
+    commonName: "Common squirrel monkey",
+    species: "Saimiri sciureus",
+    location: "Smith Jungle",
+    age: 2,
+    image: Common_squirrel_monkey.jpg
+}
+var Chester = {
+    name: "Chester",
+    commonName: "Black howler monkey",
+    species: "Alouatta caraya",
+    location: "Smith Jungle",
+    age: 5,
+    image: Black_howler_monkey.jpg
+}
+var Scarborough = {
+    name: "Scarborough",
+    commonName: "Scarlet macaw",
+    species: "Ara macao",
+    location: "Smith Jungle",
+    age: 3,
+    image: Scarlet_macaw.jpg
+}
+var Maverick = {
+    name: "Maverick",
+    commonName: "Tufted puffin",
+    species: "Fratercula cirrhata",
+    location: "Birai Aquarium",
+    age: 2,
+    image: Tufted_puffin.jpg
+}
+var Puck = {
+    name: "Puck",
+    commonName: "Little penguin",
+    species: "Eudyptula minor",
+    location: "Birai Aquarium",
+    age: 4,
+    image: Little_penguin.jpg
+}
+var Tweed = {
+    name: "Tweed",
+    commonName: "Leafy sea dragon",
+    species: "Phycodurus eques",
+    location: "Birai Aquarium",
+    age: 1,
+    image: Leafy_seadragon.jpg
+}
+var Tayler = {
+    name: "Tayler",
+    commonName: "Swift fox",
+    species: "Vulpes velox",
+    location: "Rodeheaver Desert",
+    age:9,
+    image: Swift_fox.jpg
+}
+var Trip = {
+    name: "Trip",
+    commonName: "Cape thick-knee",
+    species: "Burhinus capensus",
+    location: "Rodeheaver Desert",
+    age: 5,
+    image: Cape_thick-knee.jpg
+}
+var Helios = {
+    name: "Helios",
+    commonName: "Mantled guereza",
+    species: "Colobus guereza",
+    location: "Fagan Valley",
+    age: 11,
+    image: Mantled_guereza.jpg
+}
+var Asteria = {
+    name: "Asteria",
+    commonName: "Western lowland gorilla",
+    species: "Gorilla gorilla gorilla",
+    location: "Fagan Valley",
+    age: 5,
+    image: Western_lowland_gorilla.jpg
+}
+var Perses = {
+    name: "Perses",
+    commonName: "Wolf's mona monkey",
+    species: "Cercopithecus wolfi",
+    location: "Fagan Valley",
+    age: 10,
+    image: Wolfs_mana_monkey.jpg
+}
+var Pip = {
+    name: "Pip",
+    commonName: "Red river hog",
+    species: "Potamochoerus porcus",
+    location: "Fagan Valley",
+    age: 4,
+    image: Red_river_hog.jpg
+}
+var Skrytnaya = {
+    name: "Skrytnaya",
+    commonName: "Amur leopard",
+    species: "Panthera pardus orientalis",
+    location: "Kond Cat Complex",
+    age: 8,
+    image: Amur_leopard.jpg
+}
+var Matadi = {
+    name: "Matadi",
+    commonName: "African lion",
+    species: "Panthera leo",
+    location: "Kond Cat Complex",
+    age: 6,
+    image: African_lion.jpg
+}
+var Kimani = {
+    name: "Kimani",
+    commonName: "Siberian tiger",
+    species: "Panthera tigris altaica",
+    location: "Kond Cat Complex",
+    age: 9,
+    image: Siberian_tiger.jpg
+}
+var Antipode = {
+    name: "Antipode",
+    commonName: "Polar bear",
+    species: "Ursus maritimus",
+    location: "Moser Canyon",
+    age: 11,
+    image: Polar_bear.jpg
+}
